3c3510b8e7b290cc5e83f5ba081c36c56b15b5a5
clssrx <48386524+clssra@users.noreply.github.com>
2026-02-01 20:21:24 +0100

    sanity and next js setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# QU'OUIR
+
+QU'OUIR collective website
---
+++ apps/studio-quouir/schemaTypes/authorType.ts
@@ -0,0 +1,21 @@
+import {defineType, defineField} from 'sanity'
+
+export const authorType = defineType({
+  name: 'author',
+  title: 'Author',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'bio',
+      type: 'text',
+    }),
+    defineField({
+      name: 'photo',
+      type: 'image',
+    }),
+  ],
+})
