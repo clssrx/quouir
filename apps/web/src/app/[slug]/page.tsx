9bbefb0c7ec654930b7dc9d70c8c0036cfdf23c7
clssrx <48386524+clssra@users.noreply.github.com>
2026-02-04 17:38:03 +0100

    refactor sanity queries

--- apps/web/src/app/[slug]/page.tsx
+++ apps/web/src/app/[slug]/page.tsx
@@ -1,19 +1,11 @@
-import { PortableText, type SanityDocument } from 'next-sanity';
+import { PortableText } from 'next-sanity';
 import imageUrlBuilder from '@sanity/image-url';
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import { client } from '@/sanity/client';
 import Link from 'next/link';
 import Image from 'next/image';
-
-const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]{
-  _id,
-  title,
-  slug,
-  publishedAt,
-  body,
-	image,
-  author->{_id, name, slug}
-}`;
+import { Post, SanitySlug } from '@/types/sanity';
+import { getPostBySlug } from '@/sanity/posts';
 
 const { projectId, dataset } = client.config();
 const urlFor = (source: SanityImageSource) =>
@@ -21,52 +13,49 @@ const urlFor = (source: SanityImageSource) =>
 		? imageUrlBuilder({ projectId, dataset }).image(source)
 		: null;
 
-const options = { next: { revalidate: 30 } };
-
 export default async function PostPage({
 	params,
 }: {
-	params: Promise<{ slug: string }>;
+	params: Promise<SanitySlug>;
 }) {
-	const post = await client.fetch<SanityDocument>(
-		POST_QUERY,
-		await params,
-		options,
-	);
-	const postImageUrl = post.image
-		? urlFor(post.image)?.width(550).height(310).url()
+	const post: Post | null = await getPostBySlug(await params);
+
+	if (!post) {
+		return <p>Post not found.</p>;
+	}
+
+	const { title, body = [], publishedAt, image, author } = post;
+	const postImageUrl = image
+		? urlFor(image)?.width(550).height(310).url()
 		: null;
-
-	const postTitle = post.title || 'Untitled Post';
-	const postBody = post.body || [];
-	const postPublishedAt = post.publishedAt || '';
-
-	const authorName = post.author?.name || 'Unknown Author';
-	const authorSlug = post.author?.slug?.current || '';
-	const authorLink = authorSlug ? `/authors/${authorSlug}` : '#';
 
 	return (
 		<main className='container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4'>
 			<Link href='/' className='hover:underline'>
 				← Back to posts
 			</Link>
-			<h1 className='text-4xl font-bold '>{postTitle}</h1>
-			<Link href={authorLink} className='hover:underline'>
-				<h3 className='text-lg text-gray-600'>{authorName}</h3>
+			<h1 className='text-4xl font-bold '>{title}</h1>
+			<Link
+				href={author?.slug ? `/authors/${author.slug.current}` : '#'}
+				className='hover:underline'
+			>
+				<h3 className='text-lg text-gray-600'>
+					{author?.name || 'Unknown Author'}
+				</h3>
 			</Link>
 
-			<p>{new Date(postPublishedAt).toLocaleDateString()}</p>
+			<p>{new Date(publishedAt).toLocaleDateString()}</p>
 			{postImageUrl && (
 				<Image
 					src={postImageUrl}
-					alt={postTitle}
+					alt={title}
 					className='aspect-video rounded-xl'
 					width={550}
 					height={310}
 				/>
 			)}
 			<div className='prose text-justify'>
-				{Array.isArray(postBody) && <PortableText value={postBody} />}
+				{Array.isArray(body) && <PortableText value={body} />}
 			</div>
 		</main>
 	);
