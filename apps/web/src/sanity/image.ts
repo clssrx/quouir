20a4127f2add5510e0ef5911566f3792850f8fa9
clssrx <48386524+clssra@users.noreply.github.com>
2026-02-05 13:22:12 +0100

    refactor image helper

---
+++ apps/web/src/sanity/image.ts
@@ -0,0 +1,10 @@
+// sanity/image.ts (or wherever)
+import imageUrlBuilder from '@sanity/image-url';
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
+import { client } from './client';
+
+const builder = imageUrlBuilder(client);
+
+export function urlForImage(source: SanityImageSource) {
+	return builder.image(source);
+}
