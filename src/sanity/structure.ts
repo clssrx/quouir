48c7b6a893073a19f49909f02db09003e0e6ec50
clssrx <48386524+clssra@users.noreply.github.com>
2026-02-12 13:11:22 +0100

    update studio structure

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -1,7 +1,30 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from 'sanity/structure';
 
-// https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
-  S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+	S.list()
+		.title('Content')
+		.items([
+			S.listItem()
+				.title('Posts by category')
+				.child(
+					S.documentTypeList('category')
+						.title('Categories')
+						.child((categoryId) =>
+							S.documentList()
+								.title('Posts')
+								.filter('_type == "post" && category._ref == $categoryId')
+								.params({ categoryId })
+								.initialValueTemplates([
+									S.initialValueTemplateItem('post-by-category', {
+										categoryId,
+									}),
+								]),
+						),
+				),
+
+			S.divider(),
+
+			S.documentTypeListItem('post').title('All Posts'),
+			S.documentTypeListItem('category'),
+			S.documentTypeListItem('author'),
+		]);
