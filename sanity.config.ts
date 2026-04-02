bea503a3844eafac835a60da0287f2cee97dede6
clssrx <leon.colosio@icloud.com>
2026-04-02 12:08:07 +0200

    enable vision tool only in dev

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,23 +1,21 @@
 'use client';
 
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
- */
-
 import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
 
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from './src/sanity/env';
 import { schema } from './src/sanity/schemaTypes';
 import { structure } from './src/sanity/structure';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
+	name: 'default',
+	title: "Qu'ouir",
 	basePath: '/studio',
 	projectId,
 	dataset,
-
 	schema: {
 		...schema,
 		templates: (templates) => [
@@ -36,9 +34,8 @@ export default defineConfig({
 			},
 		],
 	},
-
 	plugins: [
 		structureTool({ structure }),
-		visionTool({ defaultApiVersion: apiVersion }),
+		...(!isProduction ? [visionTool({ defaultApiVersion: apiVersion })] : []),
 	],
 });
